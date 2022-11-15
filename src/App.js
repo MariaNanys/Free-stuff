a4fee5b57d093cbfd70d3171cce34e344df1a6df
MariaNanys <maria.iwaszczenko@gmail.com>
2022-11-15 17:08:18 +0100

    Create environmentof project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export function App() {
+  return (
+    <div>
+      <header className="header">
+          Learn React
+    </header>
+    </div>
+  );
+}
+
+export default App;
